f03fc75257dd82ed8fe116144e0cf0275291d08b
Arthur-Philiposki <arthur.5943@aluno.pr.senac.br>
2025-02-26 19:59:32 -0300

    Update temporario.py
    Adicionado o select

--- temporario.py
+++ temporario.py
@@ -2,7 +2,10 @@ import streamlit as st
 
 st.header ("Cabeçalho")
 st.toggle ("Toggle")
-st.text_area ("Enter Text")
-st.text_input ("Movie Title", "Life of Brain")
+st.text_area ("Text de caixa Grande")
+st.text_input ("Text ants da Caixa", "Texto fraco na Caixa")
+st.select (
+  'Qual a sua cor favorita?', 
+  ('Azul', 'Vermelho', 'Verde'))
 st.button ("Botão Salvar")
 
