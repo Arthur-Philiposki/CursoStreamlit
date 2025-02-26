8ae9952a167dd173615a17024a8a023dac817d8d
Arthur-Philiposki <arthur.5943@aluno.pr.senac.br>
2025-02-26 20:04:03 -0300

    Update temporario.py
    Adicionado Multiselect

--- temporario.py
+++ temporario.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
 st.header ("Cabeçalho")
+st.multiselect (
+  'Quais são suas corers favoritas?',
+  ['Verde', 'Amarelo', 'Vermelho', 'Azul'],
+  ['Amarelo', 'Vermelho',])
 st.toggle ("Toggle")
 st.text_area ("Text de caixa Grande")
 st.text_input ("Text ants da Caixa", "Texto fraco na Caixa")
