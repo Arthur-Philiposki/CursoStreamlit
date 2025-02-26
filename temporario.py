abc237cc9d0d2128c74048db0fa2f537312ca58c
Arthur-Philiposki <arthur.5943@aluno.pr.senac.br>
2025-02-26 20:10:32 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -1,15 +1,19 @@
 import streamlit as st
 
 st.header ("Cabeçalho")
+
 st.multiselect (
   'Quais são suas corers favoritas?',
   ['Verde', 'Amarelo', 'Vermelho', 'Azul'],
   ['Amarelo', 'Vermelho',])
+
 st.toggle ("Toggle")
 st.text_area ("Text de caixa Grande")
 st.text_input ("Text ants da Caixa", "Texto fraco na Caixa")
+
 st.selectbox (
   'Qual a sua cor favorita?', 
   ('Azul', 'Vermelho', 'Verde'))
+
 st.button ("Botão Salvar")
 
