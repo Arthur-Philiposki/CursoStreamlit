a06feedaa9aa6833be23713d59aaa280ae4147b2
Arthur-Philiposki <arthur.5943@aluno.pr.senac.br>
2025-02-28 20:10:29 -0300

    Update temorario2.py

--- temorario2.py
+++ temorario2.py
@@ -1,6 +1,32 @@
 import streamlit as st
 
-import streamlit as st
+st.header ("Aluno")
+
+
+st.multiselect (
+  'Quais são suas corers favoritas?',
+  ['Verde', 'Amarelo', 'Vermelho', 'Azul'],
+  ['Amarelo', 'Vermelho',])
+
+st.color_picker("Escolha a Cor", "#00f900")
+st.feedback("stars")
+
+st.toggle ("Toggle")
+st.text_area ("Text de caixa Grande")
+st.text_input ("Text ants da Caixa", "Texto fraco na Caixa")
+
+st.selectbox (
+  'Qual a sua cor favorita?', 
+  ('Azul', 'Vermelho', 'Verde'))
+
+st.checkbox ('Sorvete')
+st.checkbox ('Banana')
+st.checkbox ('Chocolate')
+
+options = ["Norte", "Sul", "Leste", "Oeste"]
+selection = st.pills("Directions", options, selection_mode="multi")
+
+st.button ("Botão Salvar")
 
 col1, col2, col3 = st.columns(3)
 
