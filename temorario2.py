e94de3ec808011ff0a2bb61e9993152e4e7c5393
Arthur-Philiposki <arthur.5943@aluno.pr.senac.br>
2025-02-28 20:15:12 -0300

    Update temorario2.py

--- temorario2.py
+++ temorario2.py
@@ -8,43 +8,6 @@ st.text_input ("CPF", "Digite o CPF do Aluno")
 
 st.text_input ("RG", "Digite o RG do Aluno")
 
-st.multiselect (
-  'Quais são suas corers favoritas?',
-  ['Verde', 'Amarelo', 'Vermelho', 'Azul'],
-  ['Amarelo', 'Vermelho',])
+st.text_input ("RA", "Digite o RA do Aluno")
 
-st.color_picker("Escolha a Cor", "#00f900")
-st.feedback("stars")
-
-st.toggle ("Toggle")
-st.text_area ("Text de caixa Grande")
-st.text_input ("Text ants da Caixa", "Texto fraco na Caixa")
-
-st.selectbox (
-  'Qual a sua cor favorita?', 
-  ('Azul', 'Vermelho', 'Verde'))
-
-st.checkbox ('Sorvete')
-st.checkbox ('Banana')
-st.checkbox ('Chocolate')
-
-options = ["Norte", "Sul", "Leste", "Oeste"]
-selection = st.pills("Directions", options, selection_mode="multi")
-
-st.button ("Botão Salvar")
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
-
-st.button ("Reset")
+st.button ("Salvar")
